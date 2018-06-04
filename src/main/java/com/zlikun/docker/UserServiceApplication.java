e2d62572f6431cab54667f18bb190092cd190c01
zlikun <zlikun-dev@hotmail.com>
2018-06-04 15:30:39 +0800

    调整docker-compose.yml配置

--- src/main/java/com/zlikun/docker/UserServiceApplication.java
+++ src/main/java/com/zlikun/docker/UserServiceApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-////生产环境中，使用Docker来配置环境变量实现动态配置
+//生产环境中，使用Docker来配置环境变量实现动态配置
+//本例调试时，在运行命令中手动添加环境变量即可
 //@PropertySource("classpath:config.properties")
 public class UserServiceApplication {
 
